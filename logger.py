ca0f2f4f41b0c072d5c99b0e8dcbbf0d37e4294c
AlexPer <alexper@list.ru>
2022-10-18 17:17:49 +0300

    точно финал

--- logger.py
+++ logger.py
@@ -2,5 +2,5 @@ from datetime import datetime as dt
 
 def result_loger(data):
     time = dt.now().strftime('%H:%M')
-    with open("logbook.txt", 'a', encoding='utf-8') as file:
+    with open("logbook.txt", 'a') as file:
         file.write(f'{time}: new entry: {data}\n')
